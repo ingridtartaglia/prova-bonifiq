6f598765c8dccabbf91ea97c806b1123162155bf
Ingrid Tartaglia <ingrid.tartaglia@gmail.com>
2025-08-13 18:02:30 -0300

    Corrigindo problema Parte1Controller

--- Services/RandomService.cs
+++ Services/RandomService.cs
@@ -6,24 +6,45 @@ namespace ProvaPub.Services
 {
 	public class RandomService
 	{
-		int seed;
-        TestDbContext _ctx;
-		public RandomService()
+        private readonly DbContextOptions<TestDbContext> _ctx;
+
+        public RandomService()
         {
-            var contextOptions = new DbContextOptionsBuilder<TestDbContext>()
-    .UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=Teste;Trusted_Connection=True;")
-    .Options;
-            seed = Guid.NewGuid().GetHashCode();
-
-            _ctx = new TestDbContext(contextOptions);
+            _ctx = new DbContextOptionsBuilder<TestDbContext>()
+                        .UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=Teste;Trusted_Connection=True;")
+                        .Options;
         }
-        public async Task<int> GetRandom()
-		{
-            var number =  new Random(seed).Next(100);
-            _ctx.Numbers.Add(new RandomNumber() { Number = number });
-            _ctx.SaveChanges();
-			return number;
-		}
 
-	}
+        public async Task<int> GetRandom()
+        {
+            using (var ctx = new TestDbContext(_ctx))
+            {
+                var random = new Random();
+                var generatedNumbers = new List<int>();
+
+                for (int i = 0; i < 100; i++)
+                {
+                    generatedNumbers.Add(random.Next(1, 100));
+                }
+
+                var existentNumbers = await ctx.Numbers
+                                            .Where(n => generatedNumbers.Contains(n.Number))
+                                            .Select(n => n.Number)
+                                            .ToListAsync();
+
+                var number = generatedNumbers.FirstOrDefault(n => !existentNumbers.Contains(n));
+
+                if (number == 0)
+                {
+                    throw new Exception("Failed to generate a unique number after 100 tries");
+                }
+
+                var newNumber = new RandomNumber { Number = number };
+                ctx.Numbers.Add(newNumber);
+                await ctx.SaveChangesAsync();
+
+                return number;
+            }
+        }
+    }
 }
